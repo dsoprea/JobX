47c99bb0903175faf2d1f664c2e2fa7d2a6e5bc3
Dustin Oprea <myselfasunder@gmail.com>
2014-08-16 01:02:40 -0400

    Made nicer.

--- mr/config/kv.py
+++ mr/config/kv.py
@@ -7,5 +7,5 @@ REPR_DATA_TRUNCATE_WIDTH = 20
 
 DEFAULT_ATOMIC_UPDATE_MAX_ATTEMPTS = 5
 
-ENCODER = lambda data: json.dumps(data)
-DECODER = lambda encoded: json.loads(encoded)
+ENCODER = json.dumps
+DECODER = json.loads
