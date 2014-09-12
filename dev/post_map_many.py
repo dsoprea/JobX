709ce33e1102076232c77aae9e56dc896ad64180
Dustin Oprea <myselfasunder@gmail.com>
2014-09-12 02:49:34 -0400

    Switched to pprint. JSON coerces keys to strings.

--- dev/post_map_many.py
+++ dev/post_map_many.py
@@ -3,6 +3,7 @@
 import sys
 import requests
 import json
+import pprint
 
 # Submit the job and wait for a result.
 
@@ -29,10 +30,7 @@ sys.stderr.write("Request ID: [%s]\n\n" %
 raw = r.json()
 result = dict(raw['result'])
 
-formatted = json.dumps(
-                result, 
-                sort_keys=True, 
-                indent=4, 
-                separators=(',', ': '))
-
-print(formatted)
+# Note that we print using Python native printer rather than printing a JSON 
+# structure (which we prefer because it's slighter lighter) because JSON will 
+# result with forcibly-stringified keys.
+pprint.pprint(result)
