e658927f3615ebde18b8807120d4a23da91be6cd
Dustin Oprea <myselfasunder@gmail.com>
2014-09-13 17:18:47 -0400

    Moved dev logic to dev script.

--- dev/start_gunicorn_dev.sh
+++ dev/start_gunicorn_dev.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cd ../mr/resources/scripts
+#MR_USE_FAKE_QUEUE=1 \
+#MR_FAKE_QUEUE_SPOOL_PATH=$CWD/../fake_spool_path \
+#MR_MULTITHREADED=0 \
 
 PYTHONPATH=test/scope \
 MR_WORKFLOW_SCOPE_FACTORY_FQ_CLASS=test_scope.WorkflowScopeFactory \
-./mr_start_gunicorn_dev
+DEBUG=1 \
+MR_WORKFLOW_NAMES=dev \
+MR_RESULT_WRITER_FQ_CLASS=mr.result_writers.file.FileResultWriter \
+../mr/resources/scripts/mr_start_gunicorn_dev
