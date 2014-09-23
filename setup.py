57ded0576466cb27ec302316fef7021924c06422
Dustin Oprea <myselfasunder@gmail.com>
2014-09-22 20:24:40 -0400

    Addedm missing script.

--- setup.py
+++ setup.py
@@ -61,5 +61,6 @@ setuptools.setup(
             'mr/resources/scripts/mr_kv_workflow_delete',
             'mr/resources/scripts/mr_kv_workflow_get',
             'mr/resources/scripts/mr_start_gunicorn_dev',
+            'mr/resources/scripts/mr_start_gunicorn_prod',
       ],
 )
