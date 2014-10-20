028dd7fa3bb91a787579da89ee0915c46786fa06
Dustin Oprea <myselfasunder@gmail.com>
2014-10-20 12:23:56 -0400

    We've removed socket removal.
    It doesn't run right when we fork.

--- mr/utility.py
+++ mr/utility.py
@@ -1,5 +1,30 @@
+import os
+
 def load_cls_from_string(fq_cls_name):
     pivot = fq_cls_name.rfind('.')
     cls_name = fq_cls_name[pivot+1:]
     m = __import__(fq_cls_name[:pivot], fromlist=[cls_name])
     return getattr(m, cls_name)
+
+
+# TODO(dustin): We likely won't be able to implement this unless we implement 
+#               the fork ourselves, directly. We won't know the final PID to 
+#               check if it's already running, and we can't remove it at the 
+#               end of the process, because atexit.register() will be run when 
+#               Gunicorn forks itself.
+#def set_pid_file():
+#    _PID_FILEPATH = '/tmp/mapreduce.pid'
+#
+#    if os.path.exists(_PID_FILEPATH) is True:
+#        with open(_PID_FILEPATH) as f:
+#            pid = int(f.read())
+#
+#        try:
+#            os.kill(pid, 0)
+#        except OSError:
+#            pass
+#        else:
+#            raise SystemError("Already running (%d).\n" % (pid,))
+#
+#    with open(_PID_FILEPATH, 'w') as f:
+#        f.write()
