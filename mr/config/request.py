225735ee8ea14a56002f097a845aa71ed2d13f2c
Dustin Oprea <myselfasunder@gmail.com>
2014-10-20 22:45:53 -0400

    Refactored cleanup.
    We now use a Queue.Queue to track requests to be cleaned-up. We were keeping this in the KV, but we kept having misses b
    etween the two servers trying to do cleanup.
    
    We might try to simply "pop" off the KV queue (when limits are added to the in-order queue functionality with *etcd*).

--- mr/config/request.py
+++ mr/config/request.py
@@ -1,13 +1,8 @@
 import os
 
-# This dictates how many results are returned, the entity lookups that'll be 
-# performed, and the number of pruning cycles that'll be performed. It should 
-# be proportional to scale.
-CLEANUP_BATCH_SIZE = int(os.environ.get('MR_CLEANUP_BATCH_SIZE', '1000'))
-
-CLEANUP_MANDATORY_QUIET_PERIOD_S = float(os.environ.get(
-                                    'MR_CLEANUP_MANDATORY_QUIET_PERIOD_S', 
-                                    '.5'))
+CLEANUP_TIMEOUT_S = float(os.environ.get(
+                        'MR_CLEANUP_MANDATORY_QUIET_PERIOD_S', 
+                        '5'))
 
 DO_CLEANUP_REQUESTS = bool(int(os.environ.get('MR_DO_CLEANUP_REQUESTS', '1')))
 
